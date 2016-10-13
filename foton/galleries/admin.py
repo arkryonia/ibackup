a6d1e2ee1cff27ea3dffecdb9aa4c86928b1aaa5
fallanzi <atamao.falilou@gmail.com>
2016-10-13 09:19:14 +0100

    fix: dev: admin.py

--- foton/galleries/admin.py
+++ foton/galleries/admin.py
@@ -3,9 +3,9 @@ from django.contrib import admin
 from foton.galleries.models import Gallery, Photo
 
 @admin.register(Gallery)
-class PlanningAdmin(admin.ModelAdmin):
-	pass
+class GalleryAdmin(admin.ModelAdmin):
+	prepopulated_fields = {'slug': ('name',)}
 
 @admin.register(Photo)
-class PlanningItemAdmin(admin.ModelAdmin):
+class GalleryItemAdmin(admin.ModelAdmin):
 	pass
